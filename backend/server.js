64f3d1c1a8fb8ca06efb192d937b78c0eed98fee
Akram Sabsob <asabsb@sal.nt>
2026-01-17 14:28:56 +0300

    Fix: import path + define __dirname for ESM

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@
 // ---------------------------------------------------------------------------
 import express from "express";
 import cors from "cors";
+import path from "path";
 import { fileURLToPath } from "url";
 import { nanoid } from "nanoid";
 import nodemailer from "nodemailer";
