2a8cf50c7a1142f0bcede9c4ad0234d467d99ffe
asabsob <asabsoub@iisal.net>
2026-03-02 23:50:14 +0300

    Create http.js

---
+++ frontend/src/utils/http.js
@@ -0,0 +1,18 @@
+import { API_BASE } from "../config/api";
+
+export async function apiFetch(path, options = {}) {
+  const token = localStorage.getItem("token");
+
+  const res = await fetch(`${API_BASE}${path}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    },
+  });
+
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) throw new Error(data?.error || `Request failed (${res.status})`);
+  return data;
+}
