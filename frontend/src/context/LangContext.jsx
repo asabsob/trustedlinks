4ef7afe53eb1e5be0e63a324be570e0a99640453
asabsob <asabsoub@iisal.net>
2026-03-03 00:14:43 +0300

    Update LangContext.jsx

--- frontend/src/context/LangContext.jsx
+++ frontend/src/context/LangContext.jsx
@@ -5,15 +5,11 @@ const LangContext = createContext();
 export function LangProvider({ children }) {
   const [lang, setLang] = useState(localStorage.getItem("lang") || "en");
 
-  // Save language preference
-  useEffect(() => {
-    localStorage.setItem("lang", lang);
-    document.dir = lang === "ar" ? "rtl" : "ltr"; // Set direction dynamically
-    document.documentElement.lang = lang;
-  }, [lang]);
 
   // Toggle language function
-  const toggleLang = () => setLang((prev) => (prev === "en" ? "ar" : "en"));
+ const toggleLang = () => {
+  setLang(lang === "en" ? "ar" : "en");
+};
 
   return (
     <LangContext.Provider value={{ lang, setLang, toggleLang }}>
