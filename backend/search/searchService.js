787ef570a2bc1b5cb9be085f3363f4a7e7f6a478
asabsob <asabsoub@iisal.net>
2026-03-28 21:01:16 +0300

    Update searchService.js

--- backend/search/searchService.js
+++ backend/search/searchService.js
@@ -2,37 +2,38 @@ import Business from "../models/Business.js";
 import { expandTerms } from "./synonyms.js";
 import LeadToken from "../models/LeadToken.js";
 
-export async function searchBusinesses(query){
-
+export async function searchBusinesses(query) {
   const terms = expandTerms(query);
 
-  const regexList = terms.map(term => new RegExp(term,"i"));
+  const regexList = terms.map((term) => new RegExp(term, "i"));
 
   const results = await Business.find({
     status: "Active",
-    $or:[
-      {name:{$in:regexList}},
-      {name_ar:{$in:regexList}},
-      {description:{$in:regexList}},
-      {keywords:{$in:regexList}},
-      {category:{$in:regexList}}
-    ]
+    $or: [
+      { name: { $in: regexList } },
+      { name_ar: { $in: regexList } },
+      { description: { $in: regexList } },
+      { keywords: { $in: regexList } },
+      { category: { $in: regexList } },
+    ],
   })
-  .limit(10)
-  .lean();
-const enrichedResults = await Promise.all(
-  results.map(async (item) => {
-    const token = await LeadToken.create({
-      businessId: String(item._id),
-      businessPhone: item.whatsapp,
-      query,
-    });
+    .limit(10)
+    .lean();
 
-    return {
-      ...item,
-      trackedLink: `${process.env.BASE_URL}/l/${token._id}`,
-    };
-  })
-);
+  const enrichedResults = await Promise.all(
+    results.map(async (item) => {
+      const token = await LeadToken.create({
+        businessId: String(item._id),
+        businessPhone: item.whatsapp,
+        query,
+      });
 
-return enrichedResults;
+      return {
+        ...item,
+        trackedLink: `${process.env.BASE_URL}/l/${token._id}`,
+      };
+    })
+  );
+
+  return enrichedResults;
+}
