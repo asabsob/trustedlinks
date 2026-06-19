e959eb58229dade51d80e954820a74f5b619c73b
asabsob <asabsoub@iisal.net>
2026-06-19 17:07:54 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -158,7 +158,7 @@ app.use(
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 300,
+  max: 5000,
   standardHeaders: true,
   legacyHeaders: false,
   message: { error: "Too many requests, please try again later." },
