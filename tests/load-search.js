71d7e4de304f5a7cd7da2c763a824dd71b5ead3a
asabsob <asabsoub@iisal.net>
2026-06-19 16:14:49 +0300

    Update load-search.js

--- tests/load-search.js
+++ tests/load-search.js
@@ -7,10 +7,6 @@ export const options = {
     { duration: "1m", target: 10 },
     { duration: "30s", target: 0 },
   ],
-  thresholds: {
-    http_req_failed: ["rate<0.01"],
-    http_req_duration: ["p(95)<2000"],
-  },
 };
 
 export default function () {
