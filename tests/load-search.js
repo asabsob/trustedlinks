fdd4482a9f968dd29cff89fe2fe9a0eb4d770e58
asabsob <asabsoub@iisal.net>
2026-06-19 18:49:22 +0300

    Update load-search.js

--- tests/load-search.js
+++ tests/load-search.js
@@ -2,12 +2,12 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export const options = {
-  stages: [
-    { duration: "1m", target: 25 },
-    { duration: "2m", target: 50 },
-    { duration: "2m", target: 100 },
-    { duration: "1m", target: 0 },
-  ],
+stages: [
+  { duration: "30s", target: 20 },
+  { duration: "30s", target: 200 },
+  { duration: "2m", target: 200 },
+  { duration: "30s", target: 0 },
+],
 };
 
 export default function () {
