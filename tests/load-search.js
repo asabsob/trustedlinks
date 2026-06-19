8ce0d6ebb37af9e21694a14338c7a434bfaaceb0
asabsob <asabsoub@iisal.net>
2026-06-19 16:06:55 +0300

    Modify load test parameters for performance tuning

--- tests/load-search.js
+++ tests/load-search.js
@@ -3,14 +3,13 @@ import { check, sleep } from "k6";
 
 export const options = {
   stages: [
+    { duration: "30s", target: 5 },
     { duration: "1m", target: 10 },
-    { duration: "2m", target: 50 },
-    { duration: "2m", target: 100 },
-    { duration: "1m", target: 0 },
+    { duration: "30s", target: 0 },
   ],
   thresholds: {
     http_req_failed: ["rate<0.01"],
-    http_req_duration: ["p(95)<1000"],
+    http_req_duration: ["p(95)<2000"],
   },
 };
 
