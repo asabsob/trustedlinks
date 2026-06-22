4e5ac7cdd44cb900e78b2a13156c78258d9588fe
asabsob <asabsoub@iisal.net>
2026-06-22 14:12:42 +0300

    Update CampaignDashboard.jsx

--- frontend/src/pages/campaign/CampaignDashboard.jsx
+++ frontend/src/pages/campaign/CampaignDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
-import useIsMobile from "../hooks/useIsMobile";
+import useIsMobile from "../../hooks/useIsMobile";
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL || "";
 
